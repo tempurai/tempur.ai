524172075aa58e66334d2d93283bab0628331f72
Lincan Li <email@lincan.li>
2026-02-01 23:02:04 +0800

    feat: initialize blog with vitepress theme
      - Add vitepress config and theme files
      - Update package.json with blog dependencies
      - Setup homepage layout and README
      - Add archives, tags, and rss support

---
+++ .vitepress/config.ts
@@ -0,0 +1,109 @@
+import { getPosts, getPostLength } from "./theme/serverUtils";
+import { buildBlogRSS } from "./theme/rss";
+import { transformerTwoslash } from "@shikijs/vitepress-twoslash";
+import mathjax3 from "markdown-it-mathjax3";
+
+async function config() {
+  return {
+    lang: "en-US",
+    title: "Tempur.ai",
+    description: "2 Cents of tempurai",
+    head: [
+      [
+        "link",
+        {
+          rel: "icon",
+          type: "image/svg",
+          href: "/horse.svg",
+        },
+      ],
+      [
+        "meta",
+        {
+          name: "author",
+          content: "Tempurai",
+        },
+      ],
+      [
+        "meta",
+        {
+          property: "og:title",
+          content: "Home",
+        },
+      ],
+      [
+        "meta",
+        {
+          property: "og:description",
+          content: "2 Cents of tempurai",
+        },
+      ],
+    ],
+    // cleanUrls: "with-subfolders",
+    lastUpdated: false,
+    themeConfig: {
+      // repo: "clark-cui/homeSite",
+      logo: "/horse.svg",
+      avator: "/avator.png",
+      search: {
+        provider: "local",
+      },
+      docsDir: "/",
+      // docsBranch: "master",
+      posts: await getPosts(),
+      pageSize: 5,
+      postLength: await getPostLength(),
+      nav: [
+        {
+          text: "Blog",
+          link: "/",
+        },
+        {
+          text: "Tags",
+          link: "/tags",
+        },
+        // {
+        //   text: "📃Archives",
+        //   link: "/archives",
+        // },
+        {
+          text: "RSS",
+          link: "https://2cents.tempur.ai/feed.xml",
+        },
+      ],
+      socialLinks: [
+        { icon: "github", link: "https://github.com/tempurai" },
+        { icon: "twitter", link: "https://twitter.com/tempur_ai" },
+        {
+          icon: {
+            svg: `<svg role="img" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="20">
+            <path d="M874.666667 375.189333V746.666667a64 64 0 0 1-64 64H213.333333a64 64 0 0 1-64-64V375.189333l266.090667 225.6a149.333333 149.333333 0 0 0 193.152 0L874.666667 375.189333zM810.666667 213.333333a64.789333 64.789333 0 0 1 22.826666 4.181334 63.616 63.616 0 0 1 26.794667 19.413333 64.32 64.32 0 0 1 9.344 15.466667c2.773333 6.570667 4.48 13.696 4.906667 21.184L874.666667 277.333333v21.333334L553.536 572.586667a64 64 0 0 1-79.893333 2.538666l-3.178667-2.56L149.333333 298.666667v-21.333334a63.786667 63.786667 0 0 1 35.136-57.130666A63.872 63.872 0 0 1 213.333333 213.333333h597.333334z" ></path>
+            </svg>`,
+          },
+          link: "mailto:rongchuancui@gmail.com",
+        },
+      ],
+      // outline: 2, //设置右侧aside显示层级
+      aside: false,
+      // blogs page show firewokrs animation
+      showFireworksAnimation: false,
+    },
+    buildEnd: buildBlogRSS,
+    markdown: {
+      theme: {
+        light: "vitesse-light",
+        dark: "vitesse-dark",
+      },
+      codeTransformers: [transformerTwoslash()],
+      config: (md: any) => {
+        md.use(mathjax3);
+      },
+    },
+    // vite: {
+    //   ssr: {
+    //     noExternal: ["vitepress-plugin-twoslash"],
+    //   },
+    // },
+  };
+}
+export default config();
---
+++ .vitepress/theme/components/ShareCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="shareCard">
+    <h1 class="title">
+      <span>Hi, I'm Tempurai</span>
+    </h1>
+    <p class="description">I am sharing of 2 cents here.</p>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+<style scoped>
+.shareCard {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 15px;
+  text-align: center;
+}
+.title {
+  display: flex;
+  align-items: center;
+  font-size: 2.2em;
+  font-weight: bold;
+}
+.description {
+  margin: 1em 0;
+}
+</style>
